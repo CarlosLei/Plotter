fe8b2ac32557ed6f06a1390e048a25b30047ed64
CarlosLei <909208402@qq.com>
2014-09-24 11:39:13 +0800

    V1.0
    副本的第一个版本.

--- plotter.cpp
+++ plotter.cpp
@@ -1,8 +1,13 @@
 #include "plotter.h"
+#include <QPushButton>
+#include <QLabel>
 
 Plotter::Plotter(QWidget *parent)
     : QWidget(parent)
 {
+    QPushButton *okBtn = new QPushButton(this);
+    okBtn->setText(tr("OK"));
+    connect(okBtn,SIGNAL(clicked()),this,SLOT(close()));
 }
 
 Plotter::~Plotter()
